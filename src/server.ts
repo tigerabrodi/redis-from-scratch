9b224c0bfef52a3436cd7912aca1569ddcf217d5
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-02 05:51:37 +0100

    setup server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
 import { createServer } from 'node:net'
+
+const server = createServer((socket) => {
+  console.log('client connected')
+
+  socket.on('data', (data) => {
+    console.log('data received: ' + data)
+    socket.write('Echo: ' + data)
+  })
+
+  socket.on('end', () => {
+    console.log('client disconnected')
+  })
+
+  socket.on('error', (err) => {
+    console.error('An error occurred:', err)
+  })
+})
+
+server.listen(8080, () => {
+  console.log('Server listening on port 8080')
+})
